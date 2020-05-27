c966b12c78858227bcff0f991bfd319b614b50fe
Joylamary <65182446+Joylamary@users.noreply.github.com>
2020-05-27 21:09:50 +0530

    Add files via upload
    Panagram & perfect panagram & perfect panagram using Java 8

--- Panagram.java
+++ Panagram.java
@@ -1,6 +1,8 @@
 package com.GS.simple;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,7 +12,8 @@ public class Panagram {
 	private static final int ALPHABET_COUNT = 26;
 
 	public static void main(String[] args) {
-		String str = "T quick brwn fox jmps v he lazy dg";
+		//String str = "T quick brwn fox jmps v he lazy dg";
+		String str = "The quick brown fox jumps over the lzy dog";
 		//isPangram();
 		System.out.println(isPerfectPangram(str));
 	}
@@ -42,7 +45,7 @@ public class Panagram {
 			System.out.print("\nThe above string is not a pangram.");
 	}
 
-	public static boolean isPerfectPangram(String str) {
+	public static boolean isPerfectPangramJava8(String str) {
 		if (str == null) {
 			return false;
 		}
@@ -56,5 +59,49 @@ public class Panagram {
 
 		return alphabetFrequencyMap.size() == ALPHABET_COUNT
 				&& alphabetFrequencyMap.values().stream().allMatch(item -> item == 1);
+	}
+	
+
+
+	public static boolean isPerfectPangram(String str) {
+		if (str == null) {
+			return false;
+		}
+		String strUpper = str.toUpperCase();
+		String formatStr= strUpper.replace(" ", "");
+		HashMap<Character, Integer> charCount = new HashMap<Character, Integer>();
+
+		boolean[] alphaList = new boolean[26];
+		int index = 0;
+		
+		for (int i = 0; i < formatStr.length(); i++) {
+			System.out.println("formatStr.charAt(i)" + formatStr.charAt(i));
+			if (formatStr.charAt(i) >= 'A' && formatStr.charAt(i) <= 'Z') {
+				index = formatStr.charAt(i) - 'A';
+				System.out.println(index);
+			} else if (formatStr.charAt(i) >= 'a' && formatStr.charAt(i) <= 'z') {
+				index = formatStr.charAt(i) - 'a';
+				// System.out.println("a = " +index);
+			}
+			alphaList[index] = true;
+		}
+		for (int i = 0; i <= 25; i++) {
+			if (alphaList[i] == false)
+				return false;
+		}
+		for(Character c: formatStr.toCharArray()){
+			if(charCount.containsKey(c)){
+				charCount.put(c, charCount.get(c)+1);
+			}else{
+				charCount.put(c,1);
+			}
+		}
+		
+		for(Entry<Character, Integer> i : charCount.entrySet()){
+			if(i.getValue() >1){
+				return false;
+			}
+		}
+		return true;
 	}
 }
